2313f77adef966aeca2ee347ae7a3fcebc05f85d
parkjin21004 <parkjin21004@gmail.com>
2023-10-18 10:48:54 +0000

    even_list finished

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ def even_list(int_list: List[int]) -> List[int]:
     Returns:
         A list of even integers.
     """    
-    # TODO: Implement even_list
-    pass
+    res_list = []
+
+    for i in int_list:
+        if i%2==0:
+            res_list.append(i)
+
+    return res_list
 
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:    
